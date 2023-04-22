e9d1a21c4b2de3ba0204aee000263acc2eda5132
Siyu Liu <siyu.java.web@gmail.com>
2023-04-22 17:30:34 +1000

    edit package.json file under client folder

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -34,30 +34,20 @@ const typeDefs = gql`
     price:Float
     date:String
   }
-  # type Auth {
-  #   token: ID!
-  #   user: User
-  # }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
   type Query {
     users: [User]
     user(username: String!): User
-    # items(username: String!): [Item]
-    # item(itemId: ID!): Item
-    # shops: [Shop]
-    # shop(shopId: ID!): Shop
-    # lists(username: String!): [List]
-    # list(listId: ID!): List
   }
 
-  # type Mutation {
-  #   addUser(username: String!, email: String!, password: String!): Auth
-  #   login(email: String!, password: String!): Auth
-  #   addThought(thoughtText: String!): Thought
-  #   addComment(thoughtId: ID!, commentText: String!): Thought
-  #   removeThought(thoughtId: ID!): Thought
-  #   removeComment(thoughtId: ID!, commentId: ID!): Thought
-  # }
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
 `;
 
 module.exports = typeDefs;
