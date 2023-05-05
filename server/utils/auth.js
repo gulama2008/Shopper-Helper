94726e853f407864d0c5de31399344fc67268b5d
Siyu Liu <siyu.java.web@gmail.com>
2023-05-05 19:35:53 +1000

    fixing bugs

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 
 const secret = process.env.JWT_SECRET;
 const expiration = process.env.JWT_EXPIRATION;
-
+console.log(secret);
+console.log(expiration);
 module.exports = {
   authMiddleware: function ({ req }) {
     let token = req.body.token || req.query.token || req.headers.authorization;
