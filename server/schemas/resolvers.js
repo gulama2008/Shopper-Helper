6ba1658559a94421ba31f5053e273292af121c99
Siyu Liu <siyu.java.web@gmail.com>
2023-05-08 21:09:29 +1000

    cleanup extra codes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -30,71 +30,46 @@ const resolvers = {
 
     login: async (parent, { username, password }) => {
       const user = await User.findOne({ username });
-
       if (!user) {
         throw new AuthenticationError("No user found with this username");
       }
-
       const correctPw = await user.isCorrectPassword(password);
-
       if (!correctPw) {
         throw new AuthenticationError("Incorrect credentials");
       }
-
       const token = signToken(user);
-
       return { token, user };
     },
+
     addList: async (parent, { username, lists }, context) => {
-      // if (context.user) {
       const list = await List.insertMany(lists);
       const listIds = list.map((e) => {
         return e._id;
       });
       const updatedUser = await User.findOneAndUpdate(
-        // { _id: context.user._id },
         { username: username },
         { $addToSet: { lists: { $each: listIds } } },
         {
           new: true,
         }
       );
-
       return updatedUser;
-      // }
-      // throw new AuthenticationError("You need to be logged in!");
     },
 
     updateShops: async (parent, { username, shops }, context) => {
-      // if (context.user) {
-      // const list = await List.insertMany(lists);
-      // const listIds = list.map((e) => {
-      //   return e._id;
-      // });
-
       const updatedUser = await User.findOneAndUpdate(
-        // { _id: context.user._id },
         {
           username: username,
-
         },
         { $set: { shops: shops } },
         {
           new: true,
         }
       );
-
       return updatedUser;
-      // }
-      // throw new AuthenticationError("You need to be logged in!");
     },
 
     updateItems: async (parent, { username, items }, context) => {
-      // if (context.user) {
-      // const list = await List.insertMany(lists);
-      // const listIds = list.map((e) => {
-      //   return e._id;
-      // });
       items.sort(function (a, b) {
         if (a.name < b.name) {
           return -1;
@@ -105,7 +80,6 @@ const resolvers = {
         return 0;
       });
       const updatedUser = await User.findOneAndUpdate(
-        // { _id: context.user._id },
         {
           username: username,
         },
@@ -114,13 +88,8 @@ const resolvers = {
           new: true,
         }
       );
-
       return updatedUser;
-      // }
-      // throw new AuthenticationError("You need to be logged in!");
     },
-
-    
   },
 };
 
